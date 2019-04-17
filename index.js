d92fc5c38de6d81da62859ec939c384fce739d06
Alexander Myers <alexander.j.myers@icloud.com>
2019-04-17 22:50:08 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){}
+
+function deleteFromObjectByKey(object, key){}
+
+function destructivelyDeleteFromObjectByKey(object, key){}
